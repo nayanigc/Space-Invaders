238a79621b8bfed096d0c14f891ec1400c7e0f63
Nayani George Clement <nayani.george-clement@etu.u-pec.fr>
2017-03-26 04:35:00 +0200

    avancement du laoder

--- load.c
+++ load.c
@@ -2,53 +2,143 @@
 
 #define BUFSIZE 2048
 #define MAXLINES (BUFSIZE / 12)
+#define MAXAPPS (MAXLINES - 1)
 
 int split_on_string(char* s, char* lines[], char* split){
 	char* line;
 	lines[0]=s;
 	int compteur = 1;
 	while((line = strpbrk (s, split)) != NULL ){
-			//printf("espace détecté : %d\n",compteur);
 			*line = '\0';
 			s = line + 1;
 			lines[compteur] = (char *) s;
-			//printf("argv[%d] = %s\n",compteur,s);
-			//printf("argv : %s\n", argv[compteur]);
 			compteur++;
 	}
 	lines[compteur] = (char*) s;
-	lines[compteur++] = NULL;
-
+	lines[compteur-1] = NULL;
+// enleve les affichage //
 	for(int i=0; i<compteur;i++){
-		printf("argv[%d]=|%s| \n",i,argv[i]);
+		printf("lines[%d]=|%s| \n",i,lines[i]);
 	}
 	
 	return EXIT_SUCCESS;
 }
 
+void create_niveau(niveau* niv){
+	niv->nombre_app =0;
+	niv->apps =  malloc(MAXAPPS*sizeof(app_vaisseau));
+}
+
+void deleted_niveau(niveau* niv){
+	free(niv->apps);
+	free(niv);
+}
 
 int load_niveau (char* filename, niveau* n_to_load){ 
+
 	int fd = open ( filename,O_RDONLY);
 	if ( fd == -1) {
-		perror (filename)
+		perror (filename);
 		return EXIT_FAILURE;
 	}
-	int buffer[BUFSIZE];
+	create_niveau(n_to_load);
+	char buffer[BUFSIZE];
 	read (fd, buffer,BUFSIZE);
 
 	char* lines[MAXLINES];
 	split_on_string(buffer, lines, "\n");
-
 	n_to_load->nombre_vaisseaux = atoi(lines[0]);
 	int i = 1;
+	char* line;
 	while((line = lines[i]) != NULL){
 		app_vaisseau app_v;
-			
+		char* items[4];
+		split_on_string(buffer, items, " ");
+		app_v.type=atoi(items[0]);
+		app_v.temps=atoi(items[1]);
+		app_v.pos_x=atoi(items[2]);
+		app_v.pos_y=atoi(items[3]);
+		n_to_load->apps[n_to_load->nombre_app]=app_v;
+		n_to_load->nombre_app++;
 	}
 
  	close(fd);
 	return EXIT_SUCCESS;
 }
+
+
+void deleted_vaisseau(vaisseau* v){
+	int i;
+	free(v->deplacement_horizontal);
+	free(v->deplacement_vertical);
+	for (i=0; i<v->h;i++){
+		free(v->dessin[i]);
+	}
+	free(v->dessin);
+	free(v);
+}
+
+int load_vaisseau (char* filename, vaisseau* v_to_load){ 
+
+	int fd = open ( filename,O_RDONLY);
+	if ( fd == -1) {
+		perror (filename);
+		return EXIT_FAILURE;
+	}
+	
+	char buffer[BUFSIZE];
+	read (fd, buffer,BUFSIZE);
+	int i = 1;
+
+	char* lines[MAXLINES];
+	char** items;
+	items = malloc(sizeof(char*)*2);
+	for (i=0; i<2;i++){
+		items[i]=malloc (sizeof(char)*8);
+	}
+	
+	split_on_string(buffer, lines, "\n");
+	
+	split_on_string(lines[0], items," ");
+	v_to_load->l = atoi(items[0]);
+	v_to_load->h = atoi(items[1]);
+
+	v_to_load->longueur_cycle = atoi(lines[1]);
+
+	items = realloc(items, (sizeof(char*) * v_to_load->longueur_cycle));
+	for(i = 2; i < v_to_load->longueur_cycle; i++){
+		items[i] = malloc(sizeof(char) * 8);
+	}
+	split_on_string(lines[2], items, " ");
+	v_to_load->deplacement_horizontal = malloc(v_to_load->longueur_cycle * sizeof(char));
+	for(i = 0; i < v_to_load->longueur_cycle; i++){
+		v_to_load->deplacement_horizontal[i] = atoi(items[i]);
+	}
+	split_on_string(lines[3], items, " ");
+	v_to_load->deplacement_vertical = malloc(v_to_load->longueur_cycle * sizeof(char));
+	for(i = 0; i < v_to_load->longueur_cycle; i++){
+		v_to_load->deplacement_vertical[i] = atoi(items[i]);
+	}
+
+	v_to_load->nombre_vie = atoi(items[4]);
+	v_to_load->frequence_tir =atof(items[5]);
+	v_to_load->vitesse_tir=atoi(items[6]);
+	v_to_load->puissance_tir=atoi(items[7]);
+	v_to_load->symbole_tir=*items[8];
+	
+	v_to_load->dessin = malloc(v_to_load->h * (sizeof(char*)));
+	for(i = 0; i < v_to_load->h; i++){
+		v_to_load->dessin[i] = malloc(v_to_load->l * sizeof(char));
+	}
+
+	for(i = 0; i < v_to_load->h; i++){
+		v_to_load->dessin[i] = items[9 + i];
+	}
+
+ 	close(fd);
+	return EXIT_SUCCESS;
+}
+
 
 
 
--- load.h
+++ load.h
@@ -1,14 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <fcntl.h>
 
-//structure d'un niveau 
-typedef struct {
-	int nombre_vaisseaux;
-	app_vaisseau* apps; 
+//structure vaisseau 
 
-}niveau;
+typedef struct{
+	int l; 
+	int h;
+	int longueur_cycle;
+	int* deplacement_horizontal ;
+	int* deplacement_vertical;
+	int nombre_vie;
+	float frequence_tir;
+	int vitesse_tir;// en cases ou en seconde
+	int puissance_tir;
+	char symbole_tir; // ex :"."
+	char** dessin;
 
+}vaisseau;
 // apparition de vaisseau 
 
 typedef struct {
@@ -18,3 +31,16 @@ typedef struct {
 	int pos_y;
 	
 }app_vaisseau;
+
+//structure d'un niveau 
+typedef struct {
+	int nombre_vaisseaux;
+	int nombre_app;
+	app_vaisseau* apps; 
+
+}niveau;
+
+int split_on_string(char* s, char* lines[], char* split);
+int load_niveau (char* filename, niveau* n_to_load);
+void deleted_niveau(niveau* niv);
+void create_niveau(niveau* niv);
